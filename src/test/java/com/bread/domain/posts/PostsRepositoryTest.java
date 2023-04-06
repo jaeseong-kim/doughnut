a6a0a7d86b998057ab6c08da91dff62fb5ffff2b
jaeseong-kim <doctorwho123@naver.com>
2023-04-07 01:42:22 +0900

    296p까지 진도

--- src/test/java/com/bread/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/bread/domain/posts/PostsRepositoryTest.java
@@ -4,14 +4,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest //기본적으로 h2 DB 실행
 public class PostsRepositoryTest {
 
@@ -19,7 +19,7 @@ public class PostsRepositoryTest {
   PostsRepository postsRepository;
 
   //단위 테스트가 끝날때마다 @After가 달린 메소드가 실행
-  @After
+  @AfterEach
   public void cleanup() {
     postsRepository.deleteAll();
   }
--- src/test/java/com/bread/web/PostsApiControllerTest.java
+++ src/test/java/com/bread/web/PostsApiControllerTest.java
@@ -14,10 +14,10 @@ import com.bread.web.dto.PostsUpdateRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -29,14 +29,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 //jpa 기능을 위해 springboottest와 testresttemplate 사용
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
 
@@ -54,7 +54,7 @@ public class PostsApiControllerTest {
 
   private MockMvc mvc;
 
-  @Before
+  @BeforeEach
   public void setup(){
     mvc = MockMvcBuilders
         .webAppContextSetup(context)
@@ -64,7 +64,7 @@ public class PostsApiControllerTest {
 
 
 
-  @After
+  @AfterEach
   public void tearDown() throws Exception {
     postsRepository.deleteAll();
   }
