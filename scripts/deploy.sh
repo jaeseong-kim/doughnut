ec031d95d84a0cce7b594e08eccb8421196d20ee
jaeseong-kim <doctorwho123@naver.com>
2023-04-11 01:25:09 +0900

    Style

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ CURRENT_PID=$(pgrep -fl doughnut | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
-if [-z "$CURRENT_PID" ]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -20,12 +20,11 @@ else
   sleep 5
 fi
 
-
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name : $JAR_NAME"
-echo "> $JAR_NAME에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NNAME 실행"
