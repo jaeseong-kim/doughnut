1a951c846e337ebabf79f55874b553b8acdfa30a
jaeseong-kim <doctorwho123@naver.com>
2023-04-11 01:02:23 +0900

    Style: appspec.yml 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,19 +22,15 @@ fi
 
 
 echo "> 새 애플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name : $JAR_NAME"
-
 echo "> $JAR_NAME에 실행권한 추가"
-
 chmod +x $JAR_NAME
 
 echo "> $JAR_NNAME 실행"
-
 nohup java -jar \
-      -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/appication-oauth.properties,/home/ubuntu/app/application-real-db.properties \
+      -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties \
       -Dspring.profiles.active=real \
       $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
