e64a513346e5f0015362dd6bf29f7e18ff5a36b9
jaeseong-kim <doctorwho123@naver.com>
2023-04-11 00:09:34 +0900

    Docs: 자동 배포

--- src/main/java/com/bread/Application.java
+++ src/main/java/com/bread/Application.java
@@ -2,12 +2,15 @@ package com.bread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication application = new SpringApplication(Application.class);
+    application.addListeners(new ApplicationPidFileWriter());
+    application.run(args);
   }
 }
